34003146f927e3a5ef533b5ed0dde9ff612528e0
Loreto <janderclander@pecdor.es>
2020-02-14 13:57:07 +0100

    14/02/2020

--- 59_SpringBootWebCalculadora/src/main/java/curso/modelo/negocio/GestorOperacion.java
+++ 59_SpringBootWebCalculadora/src/main/java/curso/modelo/negocio/GestorOperacion.java
@@ -1,12 +1,10 @@
 package curso.modelo.negocio;
 
-import org.eclipse.jdt.internal.compiler.ast.SwitchExpression;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import curso.modelo.controlador.ControladorOperacion;
 import curso.modelo.entidad.Operacion;
 import curso.modelo.persistencia.OperacionDao;
 //import curso.modelo.persistencia.OperacionDao;
@@ -16,44 +14,74 @@ public class GestorOperacion {
 
 	@Autowired
 	private OperacionDao operacionDao;
-	@Autowired
-	private Operacion ope;
-	@Autowired
-	private ControladorOperacion cont;
-	
 	
 
-	Switch(operacion){
-	case "suma":
-	}
-		
 	@Transactional
-	public double suma(double num1, double num2, double resultado) {
+	public void operar(Operacion op) {
 
+		double resultado = 0;
+		
+
+		switch (op.getOperador()) {
+		case "suma":
+			resultado = suma(op.getNum1(),op.getNum2());
+			break;
+		case "resta":
+			resultado = resta(op.getNum1(),op.getNum2());
+			break;
+		case "multiplicacion":
+			resultado = multiplicacion(op.getNum1(),op.getNum2());
+			break;
+		case "division":
+			resultado = division(op.getNum1(),op.getNum2());
+			break;
+		default:
+			System.out.println("Algo no ha ido bien");
+		}
+		
+	
+		
+		op.setResultado(resultado);
+	
+		//persistir el objeto
+		operacionDao.save(op);
+		
+	}
+	
+	public  List<Operacion> listar(){
+		return operacionDao.findAll();
+	}
+
+	public double suma(double num1, double num2) {
+		double resultado;
 		resultado = num1 + num2;
 		return resultado;
 	}
-	
-	@Transactional
-	public double resta(double num1, double num2, double resultado) {
 
+
+	public double resta(double num1, double num2) {
+		double resultado;
 		resultado = num1 - num2;
 		return resultado;
 	}
-	
-	@Transactional
-	public double multiplicacion(double num1, double num2, double resultado) {
 
-		resultado = num1 * num2;
+
+	public double multiplicacion(double num1, double num2) {
+		double resultado;
+		resultado = (num1 * num2);
 		return resultado;
 	}
-	
-	@Transactional
-	public double division(double num1, double num2, double resultado) {
 
+
+	public double division(double num1, double num2) {
+		double resultado;
 		resultado = num1 / num2;
 		return resultado;
 	}
-	
-	
+
+	public Operacion getOperacionById(int id) {
+		return operacionDao.findById(id);
+		
+	}
+
 }
--- 59_SpringBootWebCalculadora/src/main/webapp/WEB-INF/vistas/calculadora.jsp
+++ 59_SpringBootWebCalculadora/src/main/webapp/WEB-INF/vistas/calculadora.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,26 +11,34 @@
 <body>
 <h2>Bienvenides a la mejor calculadora del mundo</h2>
 
-<form action="ControladorOperacion">
-<font>Primer operando: </font><input type="number" value="num1" placeholder="primer operando">
+<form action="doOperacion">
+<font>Primer operando: </font><input type="number" value="${param.numero1 }" name="num1" placeholder="primer operando">
 <br/>
-<font>Segundo operando: </font><input type="number" value="num2" placeholder="primer operando">
+<font>Segundo operando: </font><input type="number" value="${param.numero2 }" name="num2" placeholder="primer operando">
 <br/>
 <h3>¿Que operacion quieres hacer?</h3>
 <br/>
 <select name="operador">
 <option value=""></option>
-<option value="suma">Suma</option>
-<option value="resta">Resta</option>
-<option value="multiplicacion">Multiplicacion</option>
-<option value="division">Division</option>
+<option value="suma">Sumar</option>
+<option value="resta">Restar</option>
+<option value="multiplicacion">Multiplicar</option>
+<option value="division">Dividir</option>
 </select>
 <br/>
 	<input type="submit" value="Calcular">
 </form>
 
-<!-- 
-<h3>El resultado es ${}</h3>
- -->
+<c:if test = "${not empty param.resultado }">
+	<h3>El resultado de ${param.operador} los numeros ${param.numero1} y ${param.numero2} es ${param.resultado}</h3>
+</c:if>
+
+<a href="doTabla">Ver todas las operaciones</a>
+<c:if href="doTabla"> 
+<hr>
+<h3>${}</h3>
+</c:if>
+
+
 </body>
 </html>
